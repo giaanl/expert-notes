6ae367871e294c7d69582d8729a982dc570788d9
Gianluca Paschoalotti <gianluca170703@gmail.com>
2024-02-18 13:57:53 -0300

    adjust new-note-card.tsx & note-card.tsx

--- src/app.tsx
+++ src/app.tsx
@@ -2,6 +2,7 @@ import logo from './assets/Logo-nlw-expert.svg'
 import { NewNoteCard } from './components/new-note-card'
 import { NoteCard } from './components/note-card'
 
+
 export function App() {
  return (
   <div className="mx-auto max-w-6xl my-12 space-y-6">
@@ -17,12 +18,16 @@ export function App() {
     <div className="h-px bg-slate-700"/>
       <div className="grid grid-cols-3 gap-6 auto-rows-[250px]">
         <NewNoteCard />
+        
+        <NoteCard note={{
+          date: new Date(),
+          content: 'Hello World'
+        }}/>
 
-        <NoteCard />
-
-        <NoteCard />
-
-        <NoteCard />
+        <NoteCard note={{
+          date: new Date(),
+          content: 'Hello World'
+        }}/>
       </div>
     </div>
 )
--- src/components/note-card.tsx
+++ src/components/note-card.tsx
@@ -1,20 +1,48 @@
-export function NoteCard() {
+import * as Dialog from '@radix-ui/react-dialog'
+import { formatDistanceToNow } from 'date-fns'
+import { ptBR } from 'date-fns/locale'
+import { X } from 'lucide-react'
+
+interface NoteCardProps {
+   note:{
+    date: Date
+    content: String
+   } 
+}
+
+export function NoteCard({ note }: NoteCardProps) {
     return(
-        <button className="rounded-md text-left bg-slate-800 p-5 space-y-3 outline-none overflow-hidden relative hover:ring-2 hover:ring-slate-600 focus-visible:rign-2 focus-visible:ring-lime-400">
-            <span className="text-sm font-medium text-slate-300">
-                há 2 dias
-            </span>
-            <p className="text-sm leading-6 text-slate-400">
-                O Drizzle possui um plugin do ESLint para evitar que realizemos updates ou deletes sem where...
+        <Dialog.Root>
+            <Dialog.Trigger className="rounded-md text-left bg-slate-800 flex flex-col p-5 gap-3 outline-none overflow-hidden relative hover:ring-2 hover:ring-slate-600 focus-visible:rign-2 focus-visible:ring-lime-400">
+                <span className="text-sm font-medium text-slate-300">
+                    {formatDistanceToNow(note.date, { locale: ptBR, addSuffix: true })}
+                </span>
+                <p className="text-sm leading-6 text-slate-400">
+                {note.content}
+                </p>
+                <div className="absolute bottom-0 left-0 right-0 h-1/2 bg-gradient-to-t from-black/60 to-black/0 pointer-events-none"></div>
+            </Dialog.Trigger>
 
-                Para configurar o plugin, é preciso instalar como abaixo:
-
-                O Drizzle possui um plugin do ESLint para evitar que realizemos updates ou deletes sem where...
-
-                Para configurar o plugin, é preciso instalar como abaixo:
-            </p>
-
-            <div className='absolute bottom-0 left-0 right-0 h-1/2 bg-gradient-to-t from-black/60 to-black/0 pointer-events-none'></div>
-        </button>
+            <Dialog.Portal>
+                <Dialog.Overlay className="inset-0 fixed bg-black/50"/>
+                <Dialog.Content className="fixed overflow-hidden left-1/2 top-1/2 -translate-x-1/2 -translate-y-1/2 max-w-[640px] w-full h-[60vh] bg-slate-700 rounded-md flex flex-col outline-none">
+                    <Dialog.Close className="absolute right-0 top-0 bg-slate-800 p-1.5 text-slate-400 hover:text-slate-100">
+                        <X className='size-5'/>
+                    </Dialog.Close>
+                    <div className="flex flex-1 flex-col gap-3 p-5">
+                        <span className="text-sm font-medium text-slate-300">
+                            {formatDistanceToNow(note.date, { locale: ptBR, addSuffix: true })}
+                        </span>
+                        <p className="text-sm leading-6 text-slate-400">
+                            {note.content}
+                        </p>
+                    </div>
+                    
+                    <button type="button" className="w-full bg-slate-800 text-slate-300 text-center py-4 text-sm outline-none font-medium group">
+                        Deseja <span className="text-red-400 group-hover:underline"> apagar esta nota</span>? 
+                    </button>
+                </Dialog.Content>
+            </Dialog.Portal>
+        </Dialog.Root>
     )
 }
